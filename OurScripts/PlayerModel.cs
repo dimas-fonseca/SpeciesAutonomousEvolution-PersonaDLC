c5ff93e1d7b1af429852e93100700723908d1dbd
Dimas Olympio Silveira da Fonseca <dimas.fonseca@acad.pucrs.br>
2018-06-20 07:57:01 -0300

    Add files via upload

---
+++ OurScripts/PlayerModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerModel
+{
+    public struct LegacyModel
+    {
+        public static double foods = 0;
+        public static double ran = 0;
+        public static double defended = 0;
+        public static double attacked = 0;
+    }
+
+    public struct CurrentModel
+    {
+        public static double foods = 0;
+        public static double ran = 0;
+        public static double defended = 0;
+        public static double attacked = 0;
+    }
+
+    public static void triggerBreed()
+    {
+        LegacyModel.foods += CurrentModel.foods;
+        LegacyModel.ran += CurrentModel.ran;
+        LegacyModel.defended += CurrentModel.defended;
+        LegacyModel.attacked += CurrentModel.attacked;
+        CurrentModel.foods = 0;
+        CurrentModel.ran = 0;
+        CurrentModel.defended = 0;
+        CurrentModel.attacked = 0;
+    }
+}
